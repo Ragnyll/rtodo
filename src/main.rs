90d4b9312c0801839d984d19bdaba49eca23247d
ragnyll <ragnyll@gallowzhumour.dev>
2021-05-18 00:09:11 -0500

    im talking to gitlab

---
+++ src/main.rs
@@ -0,0 +1,103 @@
+use reqwest::Result;
+use reqwest::Client;
+use reqwest::{ClientBuilder, header};
+
+use std::time::Duration;
+
+struct GitlabApiConf {
+    base_url: String,
+    access_token: String,
+    username: String,
+    timeout: Duration,
+}
+
+impl GitlabApiConf {
+    fn new(base_url: &str, access_token: &str, username: &str, timeout: Duration) -> GitlabApiConf {
+        GitlabApiConf {
+            base_url: String::from(base_url),
+            access_token: String::from(access_token),
+            username: String::from(username),
+            timeout: timeout,
+        }
+    }
+
+    fn get_base_url(&self) -> &str {
+        &self.base_url
+    }
+
+    fn get_username(&self) -> &str {
+        &self.username
+    }
+
+    fn get_timeout(&self) -> Duration {
+        self.timeout
+    }
+
+    fn get_accesss_token(&self) -> &str {
+        &self.access_token
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    const BASE_URL: &str = "https://gitlab.com/api/v4/";
+    const USERNAME: &str = "Ragnyll";
+    let timeout = Duration::new(5, 0);
+
+    let gitlab_api_conf = GitlabApiConf::new(BASE_URL, "MY_TOKEN", USERNAME, timeout);
+    let mut headers = header::HeaderMap::new();
+    headers.insert(
+        "PRIVATE-TOKEN",
+        header::HeaderValue::from_static("MY_TOKEN"),
+    );
+    let client = ClientBuilder::new()
+        .timeout(gitlab_api_conf.get_timeout())
+        .default_headers(headers)
+        .build()?;
+
+    println!("{}", determine_user_id(gitlab_api_conf, client).await);
+
+    Ok(())
+
+    // decrypt access token: default to decrypting from .password_store dir but allow override
+    // figure our the user id and cache it
+    // /users?username=<username
+    // >>> response = json.loads(requests.get('https://gitlab.com/api/v4/users?username=ragnyll', headers={'Authorization': 'access_token ...........'}).text)
+    // >>> user = json.loads(requests.get('https://gitlab.com/api/v4/users/2436873', headers={'Authorization': 'access_token .........'}).text)
+    //
+    // get projects belonging to user
+    //
+    // >>> projects = json.loads(requests.get('https://gitlab.com/api/v4/users/2436873/projects', headers={'Authorization': 'access_token ...........'}).text)
+    //
+    // Get all issues assigned to and logged by the user
+    // >>> response = json.loads(requests.get('https://gitlab.com/api/v4/issues?assignee_id=2436873', headers={'PRIVATE-TOKEN': '.........'}).text)
+    //
+    // >>> response = json.loads(requests.get('https://gitlab.com/api/v4/issues?author_id=2436873', headers={'PRIVATE-TOKEN': '.............'}).text)
+    //
+    // Merge on project title to lightweight object
+    //
+    // merge to ~/todo.md
+}
+
+async fn determine_user_id(gitlab_api_conf: GitlabApiConf, client: Client) -> String {
+    let user_url = format!(
+        "{}/users?{}",
+        gitlab_api_conf.get_base_url(),
+        gitlab_api_conf.get_username()
+    );
+    println!("{}", user_url);
+
+    match client.get(&user_url).send().await {
+        Ok(response) => {
+            if response.status().is_success() {
+                return String::from("oh hi");
+            } else {
+                println!("{}", response.status());
+                return String::from("shit");
+            }
+        }
+        _ => {
+            return String::from("That didnt work at all");
+        }
+    }
+}
