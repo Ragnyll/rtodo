19c5ed45a6b78e5e7955ba594b3370cbfe170814
ragnyll <ragnyll@gallowzhumour.dev>
2021-07-05 23:26:35 -0500

    tabs and handles 'q' key

--- src/gui/gui_main.rs
+++ src/gui/gui_main.rs
@@ -1,12 +1,13 @@
 use std::io;
+use termion::{event::Key, raw::IntoRawMode, screen::AlternateScreen};
 use tui::Terminal;
 use tui::backend::TermionBackend;
 use tui::widgets::{Block, Borders, Tabs};
 use tui::layout::{Layout, Constraint, Direction};
-use tui::text::{Span, Spans};
+use tui::text::Spans;
 use tui::style::{Style, Color};
-use tui::symbols::DOT;
-use termion::{event::Key, input::MouseTerminal, raw::IntoRawMode, screen::AlternateScreen};
+
+use super::events::events::{Event, Events};
 
 fn create_tabs() -> Tabs<'static> {
     let mut issue_types = vec![];
@@ -19,11 +20,14 @@ fn create_tabs() -> Tabs<'static> {
         .highlight_style(Style::default().bg(Color::Black))
 }
 
-pub fn display() -> Result<(), io::Error> {
+pub fn display() -> Result<(), Box<dyn std::error::Error>> {
     let stdout = io::stdout().into_raw_mode()?;
     let stdout = AlternateScreen::from(stdout);
     let backend = TermionBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
+
+    let events = Events::new();
+
     loop {
         terminal.draw(|f| {
             let chunks = Layout::default()
@@ -36,5 +40,15 @@ pub fn display() -> Result<(), io::Error> {
             let block = Block::default().title("Block 2").borders(Borders::ALL);
             f.render_widget(block, chunks[1]);
         })?;
+
+        if let Event::Input(input) = events.next()? {
+            match input {
+                Key::Char('q') => {
+                    break;
+                }
+                _ => {}
+            }
+        }
     }
+    Ok(())
 }
--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -1,1 +1,3 @@
 pub mod gui_main;
+
+mod events;
