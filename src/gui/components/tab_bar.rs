942be5df84b123f72b85f9f985f7e25b5418dadd
ragnyll <ragnyll@gallowzhumour.dev>
2021-07-10 22:28:12 -0500

    highlighting tab dynamically

--- src/gui/components/tab_bar.rs
+++ src/gui/components/tab_bar.rs
@@ -1,6 +1,6 @@
 use tui::widgets::{Block, Borders, Tabs};
 use tui::text::Spans;
-use tui::style::{Style, Color};
+use tui::style::{Style, Color, Modifier};
 
 use crate::conf::conf::Conf;
 
@@ -8,6 +8,10 @@ pub fn create_tabs(conf: &Conf) -> Tabs<'static> {
     let tab_titles = conf.get_todo_types().iter().cloned().map(Spans::from).collect();
     Tabs::new(tab_titles)
         .block(Block::default().borders(Borders::ALL).title("Todo Types"))
-        .style(Style::default().fg(Color::Cyan))
-        .highlight_style(Style::default().bg(Color::Black))
+        .style(Style::default().fg(Color::Green))
+        .highlight_style(
+            Style::default()
+                .add_modifier(Modifier::BOLD)
+                .bg(Color::Black),
+        )
 }
--- src/gui/gui_main.rs
+++ src/gui/gui_main.rs
@@ -4,19 +4,50 @@ use tui::Terminal;
 use tui::backend::TermionBackend;
 use tui::widgets::{Block, Borders};
 use tui::layout::{Layout, Constraint, Direction};
+use tui::style::{Style, Color, Modifier};
 
 use crate::conf::conf::Conf;
 use super::components;
 use super::events::events::{Event, Events};
 
+pub struct TabsState<> {
+    pub titles: Vec<String>,
+    pub index: usize,
+}
+
+impl TabsState {
+    pub fn new(titles: Vec<String>) -> TabsState {
+        TabsState { titles, index: 0 }
+    }
+    pub fn next(&mut self) {
+        self.index = (self.index + 1) % self.titles.len();
+    }
+
+    pub fn previous(&mut self) {
+        if self.index > 0 {
+            self.index -= 1;
+        } else {
+            self.index = self.titles.len() - 1;
+        }
+    }
+}
+
+struct App {
+    tabs: TabsState,
+}
+
 /// Currently only optimized for 1/4 screen
-pub fn display(conf: &Conf) -> Result<(), Box<dyn std::error::Error>> {
+pub fn display(conf: &Conf) -> Result<(), Box<dyn std::error::Error + '_>> {
     let stdout = io::stdout().into_raw_mode()?;
     let stdout = AlternateScreen::from(stdout);
     let backend = TermionBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
 
     let events = Events::new();
+
+    let mut app = App {
+        tabs: TabsState::new(conf.get_todo_types()),
+    };
 
     loop {
         terminal.draw(|f| {
@@ -26,7 +57,15 @@ pub fn display(conf: &Conf) -> Result<(), Box<dyn std::error::Error>> {
                 .split(f.size());
 
             let tabs = components::tab_bar::create_tabs(conf);
-            f.render_widget(tabs, chunks[0]);
+            let updated_tabs = tabs.select(app.tabs.index)
+            .style(Style::default().fg(Color::Cyan))
+            .highlight_style(
+                Style::default()
+                    .add_modifier(Modifier::BOLD)
+                    .bg(Color::Black),
+            );
+
+            f.render_widget(updated_tabs, chunks[0]);
             let block = Block::default().title("Block 2").borders(Borders::ALL);
             f.render_widget(block, chunks[1]);
         })?;
@@ -36,6 +75,8 @@ pub fn display(conf: &Conf) -> Result<(), Box<dyn std::error::Error>> {
                 Key::Char('q') => {
                     break;
                 }
+                Key::Char('h') => app.tabs.next(),
+                Key::Char('l') => app.tabs.previous(),
                 _ => {}
             }
         }
